51badda51b7598f4b3b8ae5f1463901248546e34
hasib787 <mr.hasib45@gmail.com>
2024-01-10 23:43:03 +0600

    use usedispatch function

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import { connect, useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from '../redux/counter/actions'
 
-function HooksCounter ()  {
+function HooksCounter() {
+  const count = useSelector((state) => state.value)
+  const dispatch = useDispatch()
 
-    const count = useSelector((state => state.value));
+  const incrementHandler = (value) => {
+    dispatch(increment(value))
+  }
 
-    return (
-        <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-          <div className="text-2xl font-semibold">{count}</div>
-          <div className="flex space-x-3">
-            <button
-              className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-              onClick={increment}
-            >
-              Increment
-            </button>
-            <button
-              className="bg-red-400 text-white px-3 py-2 rounded shadow"
-              onClick={decrement}
-            >
-              Decrement
-            </button>
-          </div>
-        </div>
-      )
-};
+  const decrementHandler = (value) => {
+    dispatch(decrement(value))
+  }
 
+  return (
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div className="text-2xl font-semibold">
+        {count} 
+      </div>
+      <div className="flex space-x-3">
+        <button
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          onClick={() => incrementHandler(5)}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-red-400 text-white px-3 py-2 rounded shadow"
+          onClick={() => decrementHandler(2)}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
 
-  
-  export default HooksCounter;
+export default HooksCounter
