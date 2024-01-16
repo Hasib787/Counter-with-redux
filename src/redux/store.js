d7bfcd4e2624d1b7f772e99f1e79971a032e7af7
hasib787 <mr.hasib45@gmail.com>
2024-01-16 23:58:03 +0600

    creating middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// create our first middleware 
+const myLogger = ;
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
