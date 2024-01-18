e39e00f1d725024cd11ca56d7e60559f1e8a69a8
hasib787 <mr.hasib45@gmail.com>
2024-01-18 23:46:09 +0600

    install redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
 import myLogger from './middlewares/myLogger'
 import logger from 'redux-logger'
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, myLogger))
+)
 
 export default store
